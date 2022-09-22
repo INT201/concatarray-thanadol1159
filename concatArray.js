b7264819ef6f77d9fbe73f98b825e2335b99243a
thanadol <thanadol.saen@kmutt.ac.th>
2022-09-23 00:55:16 +0700

    test

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,20 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  //  if (array1 === null || array1 === undefined || array2 === null || array2 === undefined) {
+  //   return undefined
+  // }
+  // if (array1 === null,undefined,0 && array2 !== null,undefined,0) {
+  //   return array2
+  // }
+  // if (array1 !== null,undefined,0 && array2 === null,undefined,0) {
+  //    return array1
+  // }
+ 
+  // if (concatArray === 0) {
+  //   return []  
+  // }
+
+  // return concat = array1.concat(array2) 
 }
 module.exports = concatArray

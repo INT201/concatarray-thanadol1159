c0eef996240f8f3c2b73f9831709564e3a6ce158
thanadol <thanadol.saen@kmutt.ac.th>
2022-09-23 23:21:12 +0700

    test

--- concatArray.js
+++ concatArray.js
@@ -4,12 +4,12 @@ function concatArray(array1, array2) {
    if (array1 === null || array1 === undefined || array2 === null || array2 === undefined) {
     return undefined
   }
-  // if (array1 === null,undefined,0 && array2 !== null,undefined,0) {
-  //   return array2
-  // }
-  // if (array1 !== null,undefined,0 && array2 === null,undefined,0) {
-  //    return array1
-  // }
+  if (array1 === null,undefined,0 && array2 !== null,undefined,0) {
+    return array2
+  }
+  if (array1 !== null,undefined,0 && array2 === null,undefined,0) {
+     return array1
+  }
  
   if (concatArray === 0) {
     return []  

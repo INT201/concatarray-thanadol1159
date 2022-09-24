2804cf3867e02aaeaaabc429bf371a9201c1d9fb
thanadol <thanadol.saen@kmutt.ac.th>
2022-09-24 15:55:36 +0700

    imal

--- concatArray.js
+++ concatArray.js
@@ -1,20 +1,55 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-   if (array1 === null || array1 === undefined || array2 === null || array2 === undefined) {
-    return undefined
+  let concatArr 
+  if (array1 === null  && array2 === null ) {
+    return concatArr =undefined
   }
-  if (array1 === null,undefined,0 && array2 !== null,undefined,0) {
-    return array2
+  if (array1 === undefined  && array2 === undefined ) {
+    return concatArr =undefined
   }
-  if (array1 !== null,undefined,0 && array2 === null,undefined,0) {
-     return array1
+  if (array1 === null && array2 === undefined) {
+    return concatArr = undefined
   }
- 
-  if (concatArray === 0) {
-    return []  
+  if (array2 === null && array1 === undefined) {
+    return concatArr = undefined
+  }
+  if (array2 === undefined) {
+    return concatArr = array1
+  }
+  if (array2 === null) {
+    return concatArr = array1
+  }
+  if (array1 === undefined) {
+    return concatArr = array2
+  }
+  if (array1 === null) {
+    return concatArr = array2
   }
 
-  return concat = array1.concat(array2) 
+  if (array1 === [] && array2 === []) {
+    return []
+  }
+  if (array1 !== null ||  undefined && array2 !== null || undefined) {
+    return concatArr = array1.concat(array2)
+  }
+
+//   switch (concatArr) {
+//     case array1 !== null || undefined ||0  && array2 === null||undefined||0:
+//          concatArr = array1
+//       break;
+//     case array1 === null || undefined ||0  && array2 !== null ||undefined ||0 :
+//          concatArr = array2
+//       break;
+//     case array1 === null || undefined ||0  && array2 === null ||undefined ||0 :
+//       concatArr = undefined
+//       break;
+//     case  array1 !== null || undefined ||0   && array2 !== null ||undefined ||0  :
+//       concatArr = array1.concat(array2) 
+//       break;
+//   }
+
+
+    return concatArr 
 }
 module.exports = concatArray

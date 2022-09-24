bda32a158f09c962040ad8f50011ac05985d006b
thanadol <thanadol.saen@kmutt.ac.th>
2022-09-24 17:40:27 +0700

    ver2

--- concatArray.js
+++ concatArray.js
@@ -1,55 +1,51 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  let concatArr 
-  if (array1 === null  && array2 === null ) {
-    return concatArr =undefined
-  }
-  if (array1 === undefined  && array2 === undefined ) {
-    return concatArr =undefined
-  }
-  if (array1 === null && array2 === undefined) {
+  
+  // if (array1 === null  && array2 === null ) {
+  //   return concatArr =undefined
+  // }
+  // if (array1 === undefined  && array2 === undefined ) {
+  //   return concatArr =undefined
+  // }
+  // if (array1 === null && array2 === undefined) {
+  //   return concatArr = undefined
+  // }
+  // if (array2 === null && array1 === undefined) {
+  //   return concatArr = undefined
+  // }
+  // if (array2 === undefined) {
+  //   return concatArr = array1
+  // }
+  // if (array2 === null) {
+  //   return concatArr = array1
+  // }
+  // if (array1 === undefined) {
+  //   return concatArr = array2
+  // }
+  // if (array1 === null) {
+  //   return concatArr = array2
+  // }
+
+  // if (array1 === [] && array2 === []) {
+  //   return []
+  // }
+  // if (array1 !== null ||  undefined && array2 !== null || undefined) {
+  //   return concatArr = array1.concat(array2)
+  // }
+
+  if (array1==undefined && array1==null && array2==undefined && array2==null){
     return concatArr = undefined
-  }
-  if (array2 === null && array1 === undefined) {
-    return concatArr = undefined
-  }
-  if (array2 === undefined) {
-    return concatArr = array1
-  }
-  if (array2 === null) {
-    return concatArr = array1
-  }
-  if (array1 === undefined) {
+}
+else if(array1==undefined || array1==null  && array2==true){
     return concatArr = array2
-  }
-  if (array1 === null) {
-    return concatArr = array2
-  }
-
-  if (array1 === [] && array2 === []) {
-    return []
-  }
-  if (array1 !== null ||  undefined && array2 !== null || undefined) {
-    return concatArr = array1.concat(array2)
-  }
-
-//   switch (concatArr) {
-//     case array1 !== null || undefined ||0  && array2 === null||undefined||0:
-//          concatArr = array1
-//       break;
-//     case array1 === null || undefined ||0  && array2 !== null ||undefined ||0 :
-//          concatArr = array2
-//       break;
-//     case array1 === null || undefined ||0  && array2 === null ||undefined ||0 :
-//       concatArr = undefined
-//       break;
-//     case  array1 !== null || undefined ||0   && array2 !== null ||undefined ||0  :
-//       concatArr = array1.concat(array2) 
-//       break;
-//   }
-
-
-    return concatArr 
+}
+else if(array2==undefined || array2==null  && array1==true){
+        return concatArr = array1
+}
+else {
+  let concatArr = array1.concat(array2)
+  return concatArr
+}
 }
 module.exports = concatArray
